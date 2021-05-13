b2a082516f0d15b00b2f053360244475791470a4
ry1jr <roya1jr@outlook.com>
2021-05-13 21:25:28 +0200

    added git to packages and files sync script

--- sync_pkgs_files.sh
+++ sync_pkgs_files.sh
@@ -17,4 +17,4 @@ dnf repoquery --qf '%{name}' --userinstalled \
  | grep -v -- '-debuginfo$' \
  | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' > ~/dotfiles/pkgs_a.lst
 
-git add . && git commit -m "modified files and packages"
+git add . && git commit . -m "modified files and packages"
