af9a4d43a9fbce5869a3b579a75348ab82139315
ry1jr <roya1jr@outlook.com>
2021-05-13 20:54:01 +0200

    add config.fish file

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+#Volta
+set -g -x VOLTA_HOME "$HOME/.volta"
+set -g -x PATH "$VOLTA_HOME/bin:$PATH"
+
+#RVM
+set -g -x PATH "$PATH:$HOME/.rvm/bin"
+
+#Starship
+starship init fish | source
+
+#SDKMAN
+set -g -x SDKMAN_DIR "/home/prince/.sdkman"
+
+#Remove fish greeting
+set fish_greeting
+
+#Custome scripts
+set -g -x PATH "$HOME/bin:$PATH"
+
+#Alias
+alias cat "bat"
+alias ls  "exa"
+
+~/dotfiles/getpkgs.sh
+~/dotfiles/rsyncfiles.sh
+
