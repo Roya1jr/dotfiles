fda979190b1b6644a442faf068e5f47c5f6b2ec3
ry1jr <roya1jr@outlook.com>
2021-05-13 21:17:01 +0200

    combined getpkgs script and resync

--- getpkgs.sh
+++ getpkgs.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+# Resync
+
+#Folders
+rsync -rW  ~/.config/omf/ ~/dotfiles/omf/
+rsync -rW ~/.vim/plugin/ ~/dotfiles/.vim/plugin/
+rsync -rW ~/.vim/autoload/ ~/dotfiles/.vim/autoload/ 
+
+#Files
+rsync  ~/.config/starship.toml ~/dotfiles/starship.toml
+rsync  ~/.vimrc ~/dotfiles/.vimrc
+rsync  ~/.config/fish/config.fish ~/dotfiles/fish/config.fish
+
+#Get Installed packages
 dnf repoquery --qf '%{name}' --userinstalled \
  | grep -v -- '-debuginfo$' \
  | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' > ~/dotfiles/pkgs_a.lst
