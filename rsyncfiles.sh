12f663a2ae02ba7ca9bbd12900ea72f523d5671e
ry1jr <roya1jr@outlook.com>
2021-05-13 21:13:22 +0200

    fixed spelling error

--- rsyncfiles.sh
+++ rsyncfiles.sh
@@ -6,6 +6,6 @@ rsync -rW ~/.vim/plugin/ ~/dotfiles/.vim/plugin/
 rsync -rW ~/.vim/autoload/ ~/dotfiles/.vim/autoload/ 
 
 #Files
-rysnc  ~/.config/starship.toml ~/dotfiles/starship.toml
+rsync  ~/.config/starship.toml ~/dotfiles/starship.toml
 rsync  ~/.vimrc ~/dotfiles/.vimrc
 rsync  ~/.config/fish/config.fish ~/dotfiles/fish/config.fish
