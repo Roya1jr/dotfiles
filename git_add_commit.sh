157b84673b64551b59130d014a67192c918983f4
ry1jr <roya1jr@outlook.com>
2021-05-13 21:58:30 +0200

    update

--- git_add_commit.sh
+++ git_add_commit.sh
@@ -3,9 +3,11 @@
 cd ~/dotfiles/
 
 if git diff-index --quiet HEAD --; then
+   echo changes &&\
    git add . && \
 	   git commit . -m "updated files and packages " && \
 	   git push
 else
-    cd
+    cd ~
+    echo no changes
 fi
