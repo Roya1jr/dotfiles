8569a4094cf2e3a537d6e2deced65d8bc7c710f2
ry1jr <roya1jr@outlook.com>
2021-06-17 17:34:16 +0200

    updated files and packages

--- sync_local.sh
+++ sync_local.sh
@@ -14,7 +14,7 @@ rsync -rW ~/dotfiles/.vim/autoload/ ~/.vim/autoload/
 #Files
 rsync ~/dotfiles/starship.toml ~/.config/starship.toml
 rsync ~/dotfiles/.vimrc  ~/.vimrc 
-rysnc ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
+rsync ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
 
 
 
