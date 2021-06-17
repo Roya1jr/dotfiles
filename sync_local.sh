bfa8f21bec43493d3f3dbc08e53880c289a7d282
ry1jr <roya1jr@outlook.com>
2021-06-17 17:29:13 +0200

    updated files and packages

---
+++ sync_local.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#Pull from remote
+cd ~/dotfiles/
+git pull
+
+# Resync
+
+#Folders
+rsync -rW ~/dotfiles/omf/ ~/.config/omf/
+rsync -rW ~/dotfiles/.vim/plugin/ ~/.vim/plugin/
+rsync -rW ~/dotfiles/.vim/autoload/ ~/.vim/autoload/
+
+#Files
+rsync ~/dotfiles/starship.toml ~/.config/starship.toml
+rsync ~/dotfiles/.vimrc  ~/.vimrc 
+rysnc ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
+
+
+
+
