a1a7389959839ab1d9cff65bb448cb72b036d009
Prince Junior Mguni <prince.mguni@outlook.com>
2021-06-21 14:53:08 +0200

    updated files and packages

--- fish/config.fish
+++ fish/config.fish
@@ -23,7 +23,7 @@ set -g -x LD_LIBRARY_PATH "LD_LIBRARY_PATH:/usr/local/lib"
 #Alias
 alias cat "bat"
 alias ls  "exa"
-
+alias bns "play -n synth brownnoise synth pinknoise mix synth sine amod 0.1 10"
 #Custom scripts and commands
 cd
 ~/dotfiles/sync_folder.sh
