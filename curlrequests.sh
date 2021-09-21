6a809b34209da813349c4e2c77872ecf6fcdf38c
Prince Junior Mguni <prince.mguni@outlook.com>
2021-09-21 09:56:57 +0200

    updated files and packages

--- curlrequests.sh
+++ curlrequests.sh
@@ -1,4 +1,5 @@
-curl -s "https://get.sdkman.io" | bash
 curl https://get.volta.sh | bash
+curl -s "https://get.sdkman.io" | bash
 curl -sSL https://get.rvm.io | bash -s stable
 curl -fsSL https://starship.rs/install.sh | bash    
+curl -fsSL https://deno.land/x/install/install.sh | sh
