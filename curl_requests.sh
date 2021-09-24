63b2b8d343d81bedfd08b8853046ca114f67b364
Prince Junior Mguni <prince.mguni@outlook.com>
2021-09-24 23:00:15 +0200

    Added omf to curl requests

--- curl_requests.sh
+++ curl_requests.sh
@@ -3,3 +3,5 @@ curl -s "https://get.sdkman.io" | bash
 curl -sSL https://get.rvm.io | bash -s stable
 curl -fsSL https://starship.rs/install.sh | bash    
 curl -fsSL https://deno.land/x/install/install.sh | sh
+curl -L https://get.oh-my.fish | fish
+
