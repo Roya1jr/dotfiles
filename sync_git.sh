78a4343919d1440f8aa3925f3ccdb8aec44fcb5e
Prince Junior Mguni <prince.mguni@outlook.com>
2021-11-11 14:13:41 +0200

    Added file to sync with git

---
+++ sync_git.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+#Folders
+rsync -rW  ~/.config/omf/ ~/dotfiles/omf/
+rsync -rW ~/.vim/plugset/ ~/dotfiles/.vim/plugset/
+
+#Files
+rsync  ~/.config/starship.toml ~/dotfiles/starship.toml
+rsync  ~/.vimrc ~/dotfiles/.vimrc
+rsync  ~/.config/fish/config.fish ~/dotfiles/fish/config.fish
+rsync  ~/.config/tidy ~/dotfiles/tidy
+#Windows
+DIR="/mnt/c/Users/"
+if [ -d "$DIR" ]; then
+#Files
+rsync  ~/.vimrc ~/winhome/.vimrc
+rsync  ~/.config/starship.toml ~/winhome/.config/starship.toml
+
+#Folders
+rsync -rW ~/.vim/autoload/ ~/winhome/vimfiles/autoload/
+rsync -rW ~/.vim/plugset/ ~/winhome/vimfiles/plugset/
+fi
+
+#Get Installed packages
+dnf repoquery --qf '%{name}' --userinstalled \
+ | grep -v -- '-debuginfo$' \
+ | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' > ~/dotfiles/pkgs_a.lst
+
+# Push files to remote
+cd ~/dotfiles/
+
+if [[ `git status --porcelain` ]]; then
+ 	git add . && \
+	git commit . -m "updated files and packages " && \
+	git push
+fi
+
+
