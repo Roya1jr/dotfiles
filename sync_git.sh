da8dfa4e8faf514bc807c25e8e872179859861cd
Prince Junior Mguni <prince.mguni@outlook.com>
2021-11-23 18:07:16 +0200

    Added doom emacs config

--- sync_git.sh
+++ sync_git.sh
@@ -3,15 +3,21 @@
 #Folders
 rsync -rW  ~/.config/omf/ ~/dotfiles/omf/
 rsync -rW ~/.vim/plugset/ ~/dotfiles/.vim/plugset/
+rsync -rW ~/.doom.d/ ~/dotfiles/.doom.d/
 
 #Files
 rsync  ~/.config/starship.toml ~/dotfiles/starship.toml
 rsync  ~/.vimrc ~/dotfiles/.vimrc
 rsync  ~/.config/fish/config.fish ~/dotfiles/fish/config.fish
 rsync  ~/.config/tidy ~/dotfiles/tidy
+
 #Windows
 DIR="/mnt/c/Users/"
 if [ -d "$DIR" ]; then
+
+#Folders
+rsync -rW ~/.doom.d/ ~/dotfiles/.doom.d/
+
 #Files
 rsync  ~/.vimrc ~/winhome/.vimrc
 rsync  ~/.config/starship.toml ~/winhome/.config/starship.toml
