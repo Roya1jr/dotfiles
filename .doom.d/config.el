dc6f30873fd942e13222410ecd05d1be9324be5c
Prince Junior Mguni <prince.mguni@outlook.com>
2021-11-23 18:31:09 +0200

    updated files and packages

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -16,7 +16,8 @@
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;;
+
+(setq doom-font "JetBrainsMono Nerd Font");
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
@@ -25,7 +26,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'ayu-dark)
+(setq doom-theme 'doom-ayu-mirage)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
