92a3eecd8664d3082ba3fe49adaaca36c4b6796c
Prince Junior Mguni <prince.mguni@outlook.com>
2022-01-02 08:00:26 +0200

    updated files and packages

--- sync_folder.sh
+++ sync_folder.sh
@@ -5,7 +5,7 @@
 #Folders
 rsync -rW  ~/.config/omf/ ~/dotfiles/omf/
 rsync -rW ~/.vim/plugin/ ~/dotfiles/.vim/plugin/
-rsync -rW ~/.vim/autoload/ ~/dotfiles/.vim/autoload/ 
+rsync -rW ~/.vim/autoload/ ~/dotfiles/.vim/autoload/
 
 #Files
 rsync  ~/.config/starship.toml ~/dotfiles/starship.toml
@@ -15,16 +15,16 @@ rsync  ~/.config/fish/config.fish ~/dotfiles/fish/config.fish
 #Get Installed packages
 dnf repoquery --qf '%{name}' --userinstalled \
  | grep -v -- '-debuginfo$' \
- | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' > ~/dotfiles/pkgs_a.lst 
+ | grep -v '^\(kernel-modules\|kernel\|kernel-core\|kernel-devel\)$' > ~/dotfiles/pkgs_a.lst
 
-# Push files to remote 
+# Push files to remote
 cd ~/dotfiles/
 
 if [[ `git status --porcelain` ]]; then
  	git add . && \
 	git commit . -m "updated files and packages " && \
 	git push &&\
-	cd 
+	cd
 else
-  cd
+  ls
 fi
