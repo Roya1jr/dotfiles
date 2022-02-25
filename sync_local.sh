c42c526197842a1a937997b08293c422e63aba71
Prince Junior Mguni <prince.mguni@outlook.com>
2022-02-25 10:09:50 +0200

    updated files and packages

--- sync_local.sh
+++ sync_local.sh
@@ -13,9 +13,6 @@ rsync -rW ~/dotfiles/.vim/autoload/ ~/.vim/autoload/
 
 #Files
 rsync ~/dotfiles/starship.toml ~/.config/starship.toml
-rsync ~/dotfiles/.vimrc  ~/.vimrc 
-rsync ~/dotfiles/fish/config.fish ~/.config/fish/config.fish 
-
-
-
-
+rsync ~/dotfiles/.vimrc  ~/.vimrc
+rsync ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+rysnc ~/dotfiles/sway/config ~/.config/sway/config
