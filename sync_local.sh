1f532e38842546637bf26415e27e9a266f941759
Prince Junior Mguni <prince.mguni@outlook.com>
2022-05-14 14:53:07 +0200

    removed omf

--- sync_local.sh
+++ sync_local.sh
@@ -7,7 +7,6 @@ git pull
 # Resync
 
 #Folders
-rsync -rW ~/dotfiles/omf/ ~/.config/omf/
 rsync -rW ~/dotfiles/.vim/plugset/ ~/.vim/plugset/
 rsync -rW ~/dotfiles/.doom.d/ ~/.doom.d/
 
@@ -15,4 +14,3 @@ rsync -rW ~/dotfiles/.doom.d/ ~/.doom.d/
 rsync ~/dotfiles/starship.toml ~/.config/starship.toml
 rsync ~/dotfiles/.vimrc  ~/.vimrc
 rsync ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-rsync ~/dotfiles/tidy ~/.config/tidy
