a69b285c9e8a9b34649d612ff27b4c44be811139
Prince Junior Mguni <prince.mguni@outlook.com>
2022-05-29 19:17:48 +0200

    updated to use neovim

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,13 @@
-#GHCUP
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ry1jr/.ghcup/bin
+if status is-interactive
+
+#Alias
+alias cat "bat"
+alias ls  "exa"
+alias vi  "nvim"
+alias vim "nvim"
+
+#Fish Settings
+set fish_greeting
 
 #Qnial
 set -g -x PATH "$PATH:$HOME/tools/qnial7/binaries/Linux"
@@ -7,45 +15,17 @@ set -g -x PATH "$PATH:$HOME/tools/qnial7/binaries/Linux"
 #J
 set -g -x PATH "$PATH:$HOME/j903/bin"
 
-#Volta
-set -g -x VOLTA_HOME "$HOME/.volta"
-set -g -x PATH "$VOLTA_HOME/bin:$PATH"
+#ASDF
+source ~/.asdf/asdf.fish
 
-#RVM
-set -g -x PATH "$PATH:$HOME/.rvm/bin"
+#Zoxide
+zoxide init fish | source
 
-#Starship
+#Starshiprs
 starship init fish | source
 
-#SDKMAN
-set -g -x SDKMAN_DIR "$HOME/.sdkman"
-
-#Remove fish greeting
-set fish_greeting
-
-#Pip
-set -g -x PATH "$PATH:$HOME/.local/bin"
-
-#Raylib
-set -g -x LD_LIBRARY_PATH "LD_LIBRARY_PATH:/usr/local/lib"
-
-#Mercury
-set -g -x PATH "$PATH:/usr/local/mercury-20.06.1/bin"
-set -g -x MANPATH "$MANPATH:/usr/local/mercury-20.06.1/man"
-set -g -x INFOPATH "$INFOPATH:/usr/local/mercury-20.06.1/info"
-
-#Alias
-alias cat "bat"
-alias ls  "exa"
-
-#Rust
-set -g -x PATH "$PATH:$HOME/.cargo/bin"
-
-#Deno
-set -g -x PATH "$PATH:$HOME/.deno/bin"
-
-#Tidy
-set -g -x HTML_TIDY "$HOME/.config/tidy"
-
 #Custom scripts and commands
-~/dotfiles/sync_folder.sh
+#~/dotfiles/sync_folder.sh
+
+end
+
