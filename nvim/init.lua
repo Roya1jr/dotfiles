240b26b90b018d0080a39eceae356119a50da51c
Prince Mguni <princem.bestforu.co.za>
2022-05-30 18:25:31 +0200

    updated files and packages

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,18 @@
-
+require "utils"
+require("plugins").setup()
+vim.cmd [[
+set clipboard+=unnamedplus
+set number 
+set encoding=utf-8
+set updatetime=300
+let &t_ut=''
+set t_Co=256
+set encoding=UTF-8
+set termguicolors
+set number
+set mouse=a
+set background=dark
+set visualbell
+set noerrorbells
+set t_vb=
+]]
