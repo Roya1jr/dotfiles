18ce163295cbf2c9820daa0810312ebdb1ac47a0
Prince Mguni <princem.bestforu.co.za>
2022-06-05 15:00:34 +0200

    sync local windows

--- sync_local.sh
+++ sync_local.sh
@@ -15,3 +15,16 @@ rsync -rW ~/dotfiles/nvim ~/.config/nvim
 rsync ~/dotfiles/starship.toml ~/.config/starship.toml
 rsync ~/dotfiles/.vimrc  ~/.vimrc
 rsync ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+
+#Windows
+DIR="/mnt/c/Users/"
+if [ -d "$DIR" ]; then
+
+#Files
+rsync  ~/dotfiles/.vimrc $winhome/.vimrc
+rsync  ~/dotfiles/starship.toml $winhome/.config/starship.toml
+
+#Folders
+rsync -rW  ~/dotfiles/.vim/plugset/ $winhome/vimfiles/plugset/
+
+fi
