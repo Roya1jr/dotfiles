61bc0140a3675c9c6bf9a594677f0082034382e7
Prince Junior Mguni <princem@bestforu.co.za>
2022-06-13 10:27:42 +0200

    added asdf

--- pkginstall.sh
+++ pkginstall.sh
@@ -2,4 +2,10 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Not running as root"
     exit
 fi
-cat pkgs_a.lst | sudo xargs dnf -y install 
+cat pkgs_a.lst | sudo xargs dnf -y install
+while read PLG
+do
+asdf plugin-add "$PLG" && asdf install "$PLG" latest && asdf global "$PLG" latest
+
+done < plugins.lst
+ 
